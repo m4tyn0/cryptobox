615c4afc1377408372ec5547a4e1f9f9a363925f
Matěj <99069615+m4tyn0@users.noreply.github.com>
2024-06-20 12:46:43 +0200

    svelte config changed

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		fs: {
-			allow: ['..'],
-		},
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
 	},
-});
+	preprocess: vitePreprocess()
+};
+export default config;
