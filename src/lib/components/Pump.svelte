9c28114864a756e2a3d50409a9f90431904c6a9a
Matěj <99069615+m4tyn0@users.noreply.github.com>
2024-06-21 08:36:17 +0200

    new components

---
+++ src/lib/components/Pump.svelte
@@ -0,0 +1,7 @@
+<div class=" fixed top-32 right-10 md:top-32 md:right-20">
+    <a href="https://pump.fun" target="_blank" rel="noopener noreferrer">
+        <img src="/images/pump.png" alt="Pump Fun" class=" h-8 w-8 md:h-16 md:w-16 animate-bounce" />
+    </a>
+     <p>Buy now</p>
+
+</div>
---
+++ src/lib/components/Socials.svelte
@@ -0,0 +1,7 @@
+    <section id="socials" class="flex flex-col justify-center items-center">
+      <ul class="flex space-x-4 justify-center">
+        <li><a href="https://twitter.com" target="_blank"><img src="/images/twitter.svg" alt="Twitter" class="h-6 w-6 invert"></a></li>
+        <li><a href="https://discord.com" target="_blank"><img src="image" alt="Discord" class="h-6 w-6 hidden"></a></li>
+        <li><a href="https://telegram.org" target="_blank"><img src="/images/telegram.svg" alt="Telegram" class="h-6 w-6"></a></li>
+      </ul>
+    </section>
